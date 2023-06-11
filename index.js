1dc20088a97d7af83e1451989a6583aaa4d18afb
Masum <masumt999.com>
2023-06-11 16:41:08 +0600

    class deleted success

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2.p1tk2ky.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -68,7 +68,12 @@ async function run() {
         res.send(result);
     });
    
-    app.
+    app.delete('/carts/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id) };
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+    });
 
 
     // Send a ping to confirm a successful connection
