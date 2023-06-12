2f481c858345a679c0c82c1d73032c4cfcbfae91
Masum <masumt999.com>
2023-06-12 14:07:30 +0600

    user deleted

--- index.js
+++ index.js
@@ -55,9 +55,26 @@ async function run() {
   });
 
 //   create an admin 
-   app.patch('users/admin/:id', async(req, res) => {
+   app.patch('/users/admin/:id', async(req, res) => {
     const id = req.params.id;
+    const filter = {_id: new ObjectId(id)};
+    const updateDoc = {
+        $set: {
+            role: 'admin'
+        },
+    };
+
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.send(result);
    })
+
+//    user delete
+app.delete('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id) };
+    const result = await usersCollection.deleteOne(query);
+    res.send(result);
+});
 
 
 //   classes related apis
